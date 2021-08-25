4c4799487c3b8e411861945cffc2d5c12e64f2db
Mohammadreza Ebrahimi Kkuzani <m.reza.ebrahimi1995@gmail.com>
2021-08-25 13:41:19 +0430

    python script added

--- pytorch-lightning/pytorch-lightning.py
+++ pytorch-lightning/pytorch-lightning.py
@@ -14,19 +14,34 @@ import numpy as np
 import tqdm
 from torch.utils.tensorboard import SummaryWriter
 
-# add pytorch lightning library
+
+# In[2]:
+
+
 import pytorch_lightning as pl
 import comet_ml
 
 
-#check if GPU is available
+# In[3]:
+
 
 gpu = print(torch.cuda.get_device_name() if torch.cuda.is_available else 'cpu')
 
-num_gpu = torch.cuda.device_count()
-print('NUmber of available GPU(s):', num_gpu)
+
+# In[16]:
+
+
+AVAIL_GPUS = min(1, torch.cuda.device_count())
+
+
+# In[17]:
+
 
 device = print('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+# In[6]:
+
 
 from torchvision import datasets
 import torchvision.transforms as transforms
@@ -37,33 +52,33 @@ transform=transforms.Compose([
 ])
 
 
-# In[11]:
+# In[7]:
 
 
 ds_train = datasets.MNIST('/home/mohammadreza/share/data', train=True, download=False, transform=transform)
 ds_test = datasets.MNIST('~/share/data', train=False, download=False, transform=transform)
 
 
-# In[12]:
+# In[8]:
 
 
-#get_ipython().system('nvidia-smi')
+get_ipython().system('nvidia-smi')
 
 
-# In[13]:
+# In[9]:
 
 
-#get_ipython().system('nvcc -V')
+get_ipython().system('nvcc -V')
 
 
-# In[16]:
+# In[10]:
 
 
 BATCH_SIZE = 256 if num_gpu else 64
 print('Number of alailable GPU: ', num_gpu)
 
 
-# In[17]:
+# In[11]:
 
 
 trainloader = torch.utils.data.DataLoader(ds_train, num_workers=2,
@@ -72,7 +87,7 @@ testloader = torch.utils.data.DataLoader(ds_test, batch_size=1000,
                                         shuffle=False, num_workers=2)
 
 
-# In[ ]:
+# In[21]:
 
 
 # defining the model with nn.Module
@@ -85,14 +100,126 @@ class MNISTModel(pl.LightningModule): # special calss allows you
         
     def forward(self, x):
         y_hat = self.l2(F.relu(self.l1(x.flatten(1))))
+        # we need to apply the layers into flatten version of input
         
         return y_hat
+    
+    '''
+    Till here is same as the method we used is from "nn.Sequenstial" to 
+    
+    "nn.Module". By follosing we define "train_step", "test_step", 
+    
+    "validation_step" and "prediction_step" by using inner mothods of
+    
+    "LightningModule"
+    '''
     
     def training_step(self, batch, batch_nb):
         x, y = batch
         loss = F.cross_entropy(self(x), y)
-        self.log('train_loss', loss, open_epoch=True, logger=True)
-        # The "self.log" method, use internal pytorch.lightning. since this
-        # is for logging intermediate values
+        self.log('train_loss', loss, on_epoch=True, logger=True)
+        # The "self.log" method, use internal pytorch.lightning facilities.
+        # since this is for logging intermediate values
+        return loss
+        
+    def validation_step(self, batch, batch_nb):
+        x, y = batch
+        loss = F.cross_entropy(self(x), y)
+        self.log('val_loss', loss)
+        return loss
+    
+    def test_step(self, batch, batch_nb):
+        x, y = batch
+        loss = F.cross_entropy(self(x), y)
+        self.log('test_loss', loss)
+        return loss
+    
+    def predict_step(self, batch, *args):
+        x, _ = batch
+        return self(x)
+    
+    def configure_optimizers(self):
+        opt = torch.optim.Adam(self.parameters(), lr=0.02)
+        return opt
+        
             
+
+
+# In[22]:
+
+
+model2 = MNISTModel()
+
+
+# In[23]:
+
+
+X_batch, y_batch = iter(trainloader).next()
+
+
+# In[24]:
+
+
+# Draw the images
+figure = plt.figure(figsize=(8, 8))
+cols, rows = 3, 3
+for i in range(1, cols * rows + 1):
+    sample_idx = torch.randint(len(ds_train), size=(1,)).item()
+    img, label = ds_train[sample_idx]
+    figure.add_subplot(rows, cols, i)
+    plt.title(f"{label}")
+    plt.axis("off")
+    plt.imshow(img.squeeze(), cmap="gray")
+plt.show()
+
+
+# **Trainer**  
+# _The most important part of the PyTorch Ligtning_  
+# It is a code that automates **traning**, **testing** and **validating** of your neural network. 
+# First you have construct an instance. Then fit it on your model.  
+# 
+# ```python
+# trainer = Trainer()
+# trianer.fit(model, train_dataloader, val_dataloader)
+# ```
+
+# In[30]:
+
+
+trainer = pl.Trainer(
+    gpus = AVAIL_GPUS,
+    max_epochs=1,
+    progress_bar_refresh_rate=20,
+    auto_select_gpus=True
+    
+)
+
+
+# In[31]:
+
+
+trainer.fit(model2, trainloader) #training
+
+
+# In[32]:
+
+
+# if you want to validate, you can also run validation
+
+trainer.validate(model2, testloader) #testing 
+
+
+# In[33]:
+
+
+p=trainer.predict(model2, testloader) #orediction on test data
+#trainer.predict(model2, trainloader) #orediction on train data
+
+
+# ### Tensorboard in PytorchLightning
+
+# In[ ]:
+
+
+
 
